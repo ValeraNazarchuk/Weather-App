3b161c2faa6c6f8c4458987136a763228630df3d
ValeraNazarchuk <valera55555nazarchuk@gmail.com>
2023-02-16 23:55:47 +0200

    finished the styles of the first block and connected the fonts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const url =
+  'https://api.weatherapi.com/v1/current.json?key=e9a5d3b74bf84418b11193028231901&q=London'
+
+async function get() {
+  const responce = await fetch(url)
+  const data = await responce.json()
+  return data
+}
+
+get().then(data => {
+  console.log(data);
+})
