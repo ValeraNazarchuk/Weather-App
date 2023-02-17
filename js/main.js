48511cfe3acb9609f1cb3b67b3cf43bb2e119de6
ValeraNazarchuk <valera55555nazarchuk@gmail.com>
2023-02-18 01:03:16 +0200

    replased names in html, added start elem in js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 'use strict'
 
-const url =
-  'https://api.weatherapi.com/v1/current.json?key=e9a5d3b74bf84418b11193028231901&q=London'
+const contentWeekday = document.querySelector('.content__top-weekday')
+const contentDate = document.querySelector('.content__top-text')
+const contentCity = document.querySelector('.content__top-city')
 
-async function get() {
-  const responce = await fetch(url)
-  const data = await responce.json()
-  return data
-}
+const contentImages = document.querySelector('.content__bottom-img')
+const contentTemp = document.querySelector('.content__bottom-temp')
+const contentWeather = document.querySelector('.content__bottom-weather')
+//-------
+const form = document.querySelector('.form')
+const formInput = document.querySelector('.form__input')
+const formBtn = document.querySelector('.form__button')
 
-get().then(data => {
-  console.log(data);
-})
+const precip = document.querySelector('.info-item-precip')
+const humidity = document.querySelector('.info-item-humidity')
+const wind = document.querySelector('.info-item-wind')
+
+const infoImages = document.querySelectorAll('.info__box-img')
+const infoWeekday = document.querySelectorAll('.info__box-weekday')
+const infoTemp = document.querySelectorAll('.info__box-temp')
+
